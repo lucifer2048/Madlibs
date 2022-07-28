e3f4d79233db24c2fa6e9ca96884b8a06b677173
Prathyush s panicker <72329610+lucifer2048@users.noreply.github.com>
2022-07-28 20:42:47 +0530

    Add files via upload

---
+++ madlibs1.py
@@ -0,0 +1,24 @@
+# madlibs
+print("Welcome to MADLIBS by Prathyush".upper())
+print("lets begin!!!".upper())
+print("Please enter the following with any words of your wish:")
+noun1 = input("Noun = ")
+noun2 = input("Another noun = ")
+noun3 = input("One more noun = ")
+verb1 = input("Verb = ")
+verb2 = input("Another verb = ")
+verb3 = input("One more verb = ")
+adj1 = input("Adjective = ")
+adj2 = input("One more adjective = ")
+
+
+madlibs = f"Hello {noun1}!!!\n\
+ So once upon a time, on a very {adj1} day,\
+\na guy named {noun2} while {verb1}ing, met a girl named {noun3}.\
+\nThey both seemed to hit it off from the very start. \
+\nWhile {noun2} liked to {verb1}, {noun3} was into {verb3}ing. \
+\nOne day while {verb2}ing both of them decided to get married and have kids. \
+\nThen they lived happily ever after. \
+\nSo {noun1} did you like the story? anyway have a {adj2} day..  "
+
+print(madlibs)
